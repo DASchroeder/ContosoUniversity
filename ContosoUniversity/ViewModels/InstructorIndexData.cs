332f0e7ec86d22ef9b8689d696fcbc5dcb202903
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-04-18 15:26:32 -0400

    read, modify
    added functionality for reading and manipulating Data

---
+++ ContosoUniversity/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class InstructorIndexData
+    {
+
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
